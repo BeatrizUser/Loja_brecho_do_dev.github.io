5bc3c57c76cc53ac68d6cc57f0be6348372ad4c7
BeatrizMachado <beatriz.machadosilva@gmail.com>
2021-10-21 16:38:15 -0300

    loja

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,8 +8,7 @@ async function UmResultado(){
     console.log(UmProduto.data)
     return UmProduto
 }
-async function LimitarResultados(){
-    limit = 5
+async function LimitarResultados(limit){
     listadeprodutos = await axios (`https://fakestoreapi.com/products?limit=${limit}`)
     console.log(listadeprodutos.data)
     return listadeprodutos
@@ -36,13 +35,31 @@ async function ObterCarrinho(){
     console.log(carrinho.data)
     return carrinho
 }
-$(function() {
-    console.log( "ready!" );
-    // AllPodutos()
-    // UmResultado()
-    // LimitarResultados()
-    // ClassificarResultadosPorOrdem("desc")   // classficação é "desc" ou "asc"
-    // ObterCategorias()
-    // ObterCategoriaEspecifica()
-    ObterCarrinho()
-});
+function gerahtmldoproduto(produto){
+    return `<div class="col-lg-4 col-md-4 all">
+    <div class="product-item">
+    <a href="#"><img src="${produto.image}" alt=""></a>
+    <div class="down-content">
+    <a href="#"><h4>${produto.title}</h4></a>
+    <h6>R$${produto.price}</h6>
+    <p>${produto.description}</p>
+    <ul class="stars">
+    <li><i class="fa fa-star"></i></li>
+    <li><i class="fa fa-star"></i></li>
+    <li><i class="fa fa-star"></i></li>
+    <li><i class="fa fa-star"></i></li>
+    <li><i class="fa fa-star"></i></li>
+    </ul>
+    <span>Reviews (${produto.rating.count})</span>
+    </div>
+    </div>
+    </div>`   
+}
+function chamarprodutos(produtos){
+    for (let produto of produtos.data){
+          let body = $(".row .grid")
+          let html = gerahtmldoproduto(produto)
+              body.append(html)
+    }
+}
+
